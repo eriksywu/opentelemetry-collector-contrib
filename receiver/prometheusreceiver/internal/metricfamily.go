e3baaf4e8b081142c00f554ef6ec5434a7c1f8e2
Erik Wu <erikwu09@gmail.com>
2025-10-17 13:57:32 -0700

    save

--- receiver/prometheusreceiver/internal/metricfamily.go
+++ receiver/prometheusreceiver/internal/metricfamily.go
@@ -249,9 +249,6 @@ func (mg *metricGroup) toExponentialHistogramDataPoints(dest pmetric.Exponential
 	tsNanos := timestampFromMs(mg.ts)
 	if mg.createdSeconds != 0 {
 		point.SetStartTimestamp(timestampFromFloat64(mg.createdSeconds))
-	} else if !removeStartTimeAdjustment.IsEnabled() {
-		// metrics_adjuster adjusts the startTimestamp to the initial scrape timestamp
-		point.SetStartTimestamp(tsNanos)
 	}
 	point.SetTimestamp(tsNanos)
 	populateAttributes(pmetric.MetricTypeHistogram, mg.ls, point.Attributes())
@@ -390,9 +387,6 @@ func (mg *metricGroup) toSummaryPoint(dest pmetric.SummaryDataPointSlice) {
 	point.SetTimestamp(tsNanos)
 	if mg.createdSeconds != 0 {
 		point.SetStartTimestamp(timestampFromFloat64(mg.createdSeconds))
-	} else if !removeStartTimeAdjustment.IsEnabled() {
-		// metrics_adjuster adjusts the startTimestamp to the initial scrape timestamp
-		point.SetStartTimestamp(tsNanos)
 	}
 	populateAttributes(pmetric.MetricTypeSummary, mg.ls, point.Attributes())
 }
@@ -404,9 +398,6 @@ func (mg *metricGroup) toNumberDataPoint(dest pmetric.NumberDataPointSlice) {
 	if mg.mtype == pmetric.MetricTypeSum {
 		if mg.createdSeconds != 0 {
 			point.SetStartTimestamp(timestampFromFloat64(mg.createdSeconds))
-		} else if !removeStartTimeAdjustment.IsEnabled() {
-			// metrics_adjuster adjusts the startTimestamp to the initial scrape timestamp
-			point.SetStartTimestamp(tsNanos)
 		}
 	}
 	point.SetTimestamp(tsNanos)
